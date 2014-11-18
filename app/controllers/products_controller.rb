53c859464d1f1330c32a9445e8546b86170b8b44
Jimmy <acsd68500@gmail.com>
2014-11-18 15:43:52 +0800

    complete product CRUD

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,51 @@
+class ProductsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+    @girls = Girl.all
+    @product = Product.new
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def find_params
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :quantity)
+  end
+end
