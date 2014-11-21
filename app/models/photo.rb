780f0448fbbb64be2196d5b24e3202c47398bb37
Jimmy <acsd68500@gmail.com>
2014-11-21 17:33:24 +0800

    fix photo missing bug

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ActiveRecord::Base
   belongs_to :product
   has_attached_file :images, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :images, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_presence :images
 end
