f7a9a7d03b18062bd077b64a191df868550c48f2
Jimmy <acsd68500@gmail.com>
2014-11-22 23:17:35 +0800

    add cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,6 @@
 class Cart < ActiveRecord::Base
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items, source: :product
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :girl
 
-  has_many :photos
+  has_many :photos, dependent: :destroy
   accepts_nested_attributes_for :photos
+
+  has_many :cart_items
+  has_many :carts, through: :cart_item
 
   def main_photo
     photos.first
