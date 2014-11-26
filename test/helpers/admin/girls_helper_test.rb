07b8a7ba207616d5d8ddca87b2ab924c4dc89d04
Jimmy <acsd68500@gmail.com>
2014-11-26 21:25:32 +0800

    remove test

--- test/helpers/admin/girls_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Admin::GirlsHelperTest < ActionView::TestCase
-end
--- test/helpers/manager/products_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Manager::ProductsHelperTest < ActionView::TestCase
-end
