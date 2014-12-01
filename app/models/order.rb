8b0a9d152a665c2f50fa1453e76eab2dda3fea90
Jimmy <acsd68500@gmail.com>
2014-12-01 12:34:09 +0800

    fix delete order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,15 +5,15 @@ class Order < ActiveRecord::Base
   before_save :set_total
 
   # validates_numericality_of :total, :greater_than => 0
-  validate :check_enough_quantity
+  # validate :check_enough_quantity
 
   protected
 
-  def check_enough_quantity
-    self.order_items.each do |item|
-      error[:base] << "#{item.product.name} is Sold out" if item.quantity > item.product.quantity
-    end
-  end
+  # def check_enough_quantity
+  #   self.order_items.each do |item|
+  #     error[:base] << "#{item.product.name} is Sold out" if item.quantity > item.product.quantity
+  #   end
+  # end
 
   def set_total
     self.total = self.order_items.map{ |i| i.amount }.sum
