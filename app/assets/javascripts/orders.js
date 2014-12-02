0c6f7daa28b4470985667a12e5eedd460df15d42
Jimmy <acsd68500@gmail.com>
2014-12-02 20:38:23 +0800

    cart can update without submit, use jQuery

---
+++ app/assets/javascripts/orders.js
@@ -0,0 +1,8 @@
+  $(function(){
+    $('.qty').change(function(){
+        console.log('qty change');
+        var money_string = '$' + ( $('.qty').val() * $('.price').html() ) + ' NTD';
+        $('.totalMoney').html(money_string);
+      }
+    )
+  })
