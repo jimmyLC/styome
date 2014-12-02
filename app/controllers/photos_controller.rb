b135b00836962fd877e23e41d16e9c79135b8123
Jimmy <acsd68500@gmail.com>
2014-12-02 21:06:47 +0800

    photos controller CRUD

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,36 @@
+class PhotosController < ApplicationController
+  before_action :find_photo, except: :create
+  before_action :find_product
+
+  def create
+    @photo.build(photo_params)
+    if @photo.save
+      redirect_to product_path(@product)
+    end
+  end
+
+  def update
+    if @photo.save
+      redirect_to product_path(@product)
+    end
+  end
+
+  def destroy
+    @photo.destroy
+    redirect_to product_path(@product)
+  end
+
+  private
+
+  def find_photo
+    @photo = @product.photos
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:images)
+  end
+end
