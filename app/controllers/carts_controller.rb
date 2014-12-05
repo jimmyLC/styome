5ee107037774ae2dca270442927096e5cd85f0c1
Jimmy <acsd68500@gmail.com>
2014-12-05 11:19:00 +0800

    add flash message

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,11 +15,13 @@ class CartsController < ApplicationController
   def create
     current_cart.add_item(params[:product_id], params[:quantity] || 1)
     redirect_to :back
+    flash[:notice] = "商品已加入購物車"
   end
 
   def destroy
     pid = params[:product_id]
     current_cart.remove(pid)
+    flash[:notice] = "remove successful"
 
     redirect_to :back
   end
